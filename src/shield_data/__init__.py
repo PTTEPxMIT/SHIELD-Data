3fbd4f82454d9693a5061ef32217b911a20d26cf
jhdark <james.dark.95@gmail.com>
2025-11-05 13:08:26 -0500

    add new files to init

--- src/shield_data/__init__.py
+++ src/shield_data/__init__.py
@@ -1,3 +1,7 @@
+from .build_catalogue import build_catalogue
 from .data_upload_handler import upload_data_from_folder
 
-__all__ = ["upload_data_from_folder"]
+__all__ = [
+    "build_catalogue",
+    "upload_data_from_folder",
+]
